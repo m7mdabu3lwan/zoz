773b0dbd3d916ff7d0cdef25217c4653498f7bde
Reem <94222468+reemsk2@users.noreply.github.com>
2022-11-25 13:59:24 +0200

    vfdgvfdgfdgfdg

--- app/src/main/java/com/example/zoz/signup.java
+++ app/src/main/java/com/example/zoz/signup.java
@@ -7,13 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link signup#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class signup extends Fragment {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class signup extends Fragment
+{
+    EditText email ,password,confrimpassword;
+    Button btn;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,14 +32,6 @@ public class signup extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment signup.
-     */
     // TODO: Rename and change types and number of parameters
     public static signup newInstance(String param1, String param2) {
         signup fragment = new signup();
@@ -60,5 +56,58 @@ public class signup extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        connectcomponents();
+    }
+
+    private void connectcomponents()
+    {
+        email=getView().findViewById(R.id.emailsign);
+        password=getView().findViewById(R.id.passsign);
+        confrimpassword=getView().findViewById(R.id.passconsign);
+        btn=getView().findViewById(R.id.btnsign);
+    }
+
+    public void login(View view)
+    {
+        String etemail,etpassword,etconfrimpassword;
+        etconfrimpassword=confrimpassword.getText().toString();
+        etemail=email.getText().toString();
+        etpassword=password.getText().toString();
+        if(etemail.trim().isEmpty()||etpassword.trim().isEmpty())
+        {
+            Toast.makeText(getActivity(), "some fields are missing!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(!isEmailValid(etemail))
+        {
+            Toast.makeText(getActivity(), "email is incorrect!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(!isPasswordValid(etpassword))
+        {
+            Toast.makeText(getActivity(), "password is incorrect!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+    public boolean isEmailValid(String str)
+    {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+    public boolean isPasswordValid(String password)
+    {
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 }
